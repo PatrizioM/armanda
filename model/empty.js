2e59529f516c839cf5258d8756beb28249efbabd
PatrizioM <51174001+PatrizioM@users.noreply.github.com>
2023-09-15 16:40:57 +0200

    Add files via upload

---
+++ model/empty.js
@@ -0,0 +1,39 @@
+var model = {
+
+    "version" : 2,
+    
+    "scale" : 1000.000000,
+
+    "materials": [	{
+    	"DbgColor" : 15658734,
+    	"DbgIndex" : 0,
+    	"DbgName" : "phong1SG",
+    	"colorAmbient" : [0.0, 0.0, 0.0],
+    	"colorDiffuse" : [0.8, 0.8, 0.8],
+    	"colorSpecular" : [1.0, 1.0, 1.0],
+    	"illumination" : 4,
+    	"mapDiffuse" : "ginger_color.jpg",
+    	"opticalDensity" : 1.0,
+    	"specularCoef" : 98.0
+	}],
+
+    "vertices": [],
+    
+    "morphTargets": [],
+
+    "morphColors": [],
+
+    "normals": [],
+
+    "colors": [],
+
+    "uvs": [[]],
+
+    "faces": [],
+
+    "edges" : []
+
+};
+
+postMessage( model );
+close();
